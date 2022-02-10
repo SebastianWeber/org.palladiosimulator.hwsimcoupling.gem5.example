916fc02ac5e3d8a5f7fa98732f70d4acd164df3c
Sebastian Weber <uwmgy@student.kit.edu>
2022-02-10 16:39:52 +0100

    refactoring

--- container/build/extract.py
+++ container/build/extract.py
@@ -21,23 +21,37 @@ with open("m5out/ftrace.system.cpu", "r") as trace:
             if not "_end" in line:
                 trace_cleaned.write(line)
 
-search = "system.clk_domain.clock"
-stat = ""
+searchcpu = "system.clk_domain.clock"
+searchhddread = "system.mem_ctrl.bytesReadSys"
+searchhddwrite = "system.mem_ctrl.bytesWrittenSys"
+cpu = ""
+hddread = ""
+hddwrite = ""
 with open("m5out/stats.txt", "r") as trace:
     while True:
             line = trace.readline()
-            if search in line:
-                stat = line
+            if searchcpu in line:
+                cpu = line
+            if searchhddread in line:
+                hddread = line
+            if searchhddwrite in line:
+                hddwrite = line
             if not line:
                 break
 
 with open("m5out/ftrace.system.cpu.main", "w") as trace_main:
     trace_main.write(first)
     trace_main.write(last)
-    trace_main.write(stat)
+    trace_main.write(cpu)
+    
+    cpustripped = str((int(last.split(":")[0]) - int(first.split(":")[0])) / int(cpu.replace("system.clk_domain.clock", "").replace("# Clock period in ticks (Tick)", "").strip()))
+    
+    hddreadstripped = str(int(hddread.replace("system.mem_ctrl.bytesReadSys", "").replace("# Total read bytes from the system interface side (Byte)", "").strip()))
+    
+    hddwritestripped = str(int(hddwrite.replace("system.mem_ctrl.bytesWrittenSys", "").replace("# Total written bytes from the system interface side (Byte)", "").strip()))
 
-    trace_main.write(str((int(last.split(":")[0]) - int(first.split(":")[0])) / int(stat.replace("system.clk_domain.clock", "").replace("# Clock period in ticks (Tick)", "").strip())))
+    trace_main.write(cpustripped)
 
-    print("Demand:" + str((int(last.split(":")[0]) - int(first.split(":")[0])) / int(stat.replace("system.clk_domain.clock", "").replace("# Clock period in ticks (Tick)", "").strip())))
+    print("Demand:" + "CPU:" + cpustripped + ";" + "HDD:" + hddreadstripped + hddwritestripped)
 
 
